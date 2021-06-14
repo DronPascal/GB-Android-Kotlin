bebfbd8214a055449b9257b093a5e9f1d65048c6
Andrew <shpuntik0902@gmail.com>
2021-06-14 04:02:28 +0300

    Init
    Initial commit + some changes

---
+++ WeatherApp/app/src/main/java/com/pascal/weatherapp/ui/main/PlaceholderFragment.kt
@@ -0,0 +1,31 @@
+package com.pascal.weatherapp.ui.main
+//
+//import android.os.Bundle
+//import androidx.fragment.app.Fragment
+//import androidx.lifecycle.ViewModelProvider
+//
+//class PlaceholderFragment : Fragment() {
+//
+//    private lateinit var pageViewModel: PageViewModel
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java).apply {
+//            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
+//        }
+//    }
+//
+//    companion object {
+//
+//        private const val ARG_SECTION_NUMBER = "section_number"
+//
+//        @JvmStatic
+//        fun newInstance(sectionNumber: Int): PlaceholderFragment {
+//            return PlaceholderFragment().apply {
+//                arguments = Bundle().apply {
+//                    putInt(ARG_SECTION_NUMBER, sectionNumber)
+//                }
+//            }
+//        }
+//    }
+//}
